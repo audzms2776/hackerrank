341f5e1039eab8d725c12f550f514db421a33964
audzms2776 <diddydwns135@gmail.com>
2018-10-26 13:00:05 +0900

    Create SockMerchant.py

---
+++ Warm-upChallenges/SockMerchant.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    result = 0
+    num_arr = []
+    
+    for item in ar:
+        if item in num_arr:
+            result += 1
+            del num_arr[num_arr.index(item)]
+        else:
+            num_arr.append(item)
+    
+    return result
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
