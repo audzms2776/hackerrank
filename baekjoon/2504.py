247909b2d7a97efe87bb2c22b9f2a5cdd1d2765f
audzms2776 <diddydwns135@gmail.com>
2018-11-07 00:35:14 +0900

    Update 2504.py

--- baekjoon/2504.py
+++ baekjoon/2504.py
@@ -1,8 +1,40 @@
+def is_int(i):
+    try:
+        int(i)
+        return True
+    except:
+        return False
+
+
+def is_good(ss):
+    temp_stack = []
+
+    try:
+        for c in ss:
+            if c == '(' or c == '[':
+                temp_stack.append(c)
+            elif c == ')' and temp_stack[-1] == '(':
+                temp_stack.pop()
+            elif c == ']' and temp_stack[-1] == '[':
+                temp_stack.pop()
+            else:
+                return False
+
+        return True
+    except:
+        return False
+
+
 s = input()
+
+if not is_good(s):
+    print(0)
+    exit()
+
 m_stack = []
 
 for i in s:
-    if len(s) == 0:
+    if i == '(' or i =='[' or is_int(i) :
         m_stack.append(i)
     else:
         temp = m_stack[-1]
@@ -14,8 +46,21 @@ for i in s:
             m_stack.pop()
             m_stack.append(3)
         elif i == ')':
-            temp_num = temp
             m_stack.pop()
             m_stack.pop()
-            
-    print(m_stack)
+            m_stack.append(int(temp) * 2)
+        elif i == ']':
+            m_stack.pop()
+            m_stack.pop()
+            m_stack.append(int(temp) * 3)
+
+    if len(m_stack) > 1 and is_int(m_stack[-1]) and is_int(m_stack[-2]):
+        temp_sum = m_stack[-1] + m_stack[-2]
+        m_stack.pop()
+        m_stack.pop()
+        m_stack.append(temp_sum)
+
+    # print(m_stack)
+
+print(m_stack[0])
+
